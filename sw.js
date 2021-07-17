e4170507ce9cba795020184202191b19d7a7d7dc
MatsM16 <matdyr23@gmail.com>
2021-07-17 03:02:32 +0200

    More relative path fixes

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
 const CACHE_NAME = "party-v1";
 const PRE_CACHE = [
-    ".",
-    "index.html",
-    "data/settings.json",
-    "scripts/site.js",
-    "styles/_guests.css",
-    "styles/_music.css",
-    "styles/_wc.css",
-    "styles/_wifi.css",
-    "styles/settings.css",
-    "styles/site.css",
+    "./",
+    "./index.html",
+    "./data/settings.json",
+    "./scripts/site.js",
+    "./styles/_guests.css",
+    "./styles/_music.css",
+    "././styles/_wc.css",
+    "./styles/_wifi.css",
+    "./styles/settings.css",
+    "./styles/site.css",
     "https://fonts.googleapis.com/css2?family=Roboto+Mono&family=Rubik&display=swap"
 ];
 const NETWORK_FIRST = [
